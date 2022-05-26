fe67d999d3709118529b846aefeb254cde9db501
Vinicius-Maia-A <vinimaiaazevedo13@gmail.com>
2022-05-25 21:23:25 -0300

    Printing possible moves

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -41,6 +41,13 @@ public class ChessMatch {
         board.placePiece(p,target);
         return capturedPiece;
     }
+
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
